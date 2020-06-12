fb3d45e1a0d65b69ca731c0293113c2bbaccdb3c
Julian R <56316686+julianmrodri@users.noreply.github.com>
2020-06-12 13:17:38 -0300

    fix: migrations script compatible with truffle

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.5.11;
 
 contract Migrations {
   address public owner;
-  uint public lastCompletedMigration;
+  uint public last_completed_migration;
 
   constructor() public {
     owner = msg.sender;
@@ -12,7 +12,7 @@ contract Migrations {
     if (msg.sender == owner) _;
   }
 
-  function setCompleted(uint completed) external restricted {
-    lastCompletedMigration = completed;
+  function setCompleted(uint completed) public restricted {
+    last_completed_migration = completed;
   }
 }
