2bd1c797d3c7be6472ee00484c857396ec0c5bdc
Julian R <56316686+julianmrodri@users.noreply.github.com>
2020-08-19 15:38:12 -0300

    feat: setting resolver for domain buyer

--- contracts/RNSSimplePlacementsV1.sol
+++ contracts/RNSSimplePlacementsV1.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.5.17;
 import "./ERC721SimplePlacementsV1.sol";
 import "@rsksmart/rns-registry/contracts/AbstractRNS.sol";
 import "@rsksmart/rns-rskregistrar/contracts/AbstractNodeOwner.sol";
+import "@ensdomains/resolver/contracts/profiles/AddrResolver.sol";
+
 
  /**
  * @title RNSSimplePlacementsV1
@@ -13,10 +15,12 @@ contract RNSSimplePlacementsV1 is ERC721SimplePlacementsV1 {
   bytes32 constant RSK_HASH = 0x0cd5c10192478cd220936e91293afc15e3f6de4d419de5de7506b679cbdd8ec4;
 
   AbstractRNS public rns;
+  AddrResolver public resolver;
 
-  function initialize(IERC721 _token, address owner, AbstractRNS _rns) public initializer {
+  function initialize(IERC721 _token, address owner, AbstractRNS _rns, AddrResolver _resolver) public initializer {
     ERC721SimplePlacementsV1.initialize(_token, owner);
     rns = _rns;
+    resolver = _resolver;
   }
 
   //_transferToNewOwner: Tranfers the token to the new owner.
@@ -25,7 +29,8 @@ contract RNSSimplePlacementsV1 is ERC721SimplePlacementsV1 {
     AbstractNodeOwner nodeOwner = AbstractNodeOwner(address(token));
     nodeOwner.transferFrom(owner, address(this), tokenId);
     nodeOwner.reclaim(tokenId, address(this));
-    rns.setResolver(keccak256(abi.encodePacked(RSK_HASH, bytes32(tokenId))), address(0));
+    rns.setResolver(keccak256(abi.encodePacked(RSK_HASH, bytes32(tokenId))), address(resolver));
+    resolver.setAddr(keccak256(abi.encodePacked(RSK_HASH, bytes32(tokenId))), newOwner);
     rns.setOwner(keccak256(abi.encodePacked(RSK_HASH, bytes32(tokenId))), newOwner);
     nodeOwner.transferFrom(address(this), newOwner, tokenId);
   }
